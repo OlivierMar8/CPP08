7d2dcd6ac521148e716e01ea6c24a1807b293f69
olmartin <olmartin@student.42lausanne.ch>
2022-11-30 15:15:34 +0100

    ex00 debut

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,10 @@
+//easyfind.hpp	CPP08	ex00
+
+#include <algorithm>
+
+template<typename T>
+bool	easyfind( T & cont, int n) {
+
+
+	std::T::const_iterator it = std::begin(cont);
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,11 @@
+//main.cpp	CPP08	ex00
+
+#include <iostream>
+#include <vector>
+#include "easyfind.hpp"
+
+int	main( void ) {
+
+
+	return 0;
+}
