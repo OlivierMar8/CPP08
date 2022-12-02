a8287aa94a340c13f15f3360dec5a6d78a996d52
olmartin <olmartin@student.42lausanne.ch>
2022-12-02 10:50:25 +0100

    ex00 OK

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,13 +1,22 @@
 //easyfind.hpp	CPP08	ex00
+#ifndef EASYFIND_HPP
 
 #include <algorithm>
+#include <iterator>
+
+# define EASYFIND_HPP
 
 template<typename T>
 bool	easyfind( T & cont, int n) {
 
 (void)cont;
 (void)n;
-//	std::T::consit_iterator it = std::begin(cont);
-
-	return true;
+	typename T::iterator it;
+	it = std::find( cont.begin(), cont.end(), n);
+	if (it != cont.end())
+		return true;
+	else
+		return false;
 }
+
+#endif
